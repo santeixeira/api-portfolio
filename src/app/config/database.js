f826015135edd064b0bb53ed6f2b8483faaefb38
San <jbsanthiago@gmail.com>
2022-05-11 14:44:55 -0300

    Correcao de vulnerabilidade do .env

--- src/app/config/database.js
+++ src/app/config/database.js
@@ -1,11 +1,13 @@
 import { mongoose } from "mongoose";
+import { config } from "dotenv";
+
+config();
 
 export default function connectionDb() {
-  mongoose.connect("mongodb+srv://sil:6zex9sPMGO2Wx576@sil.qcj3d.mongodb.net/sil-plants?");
+  mongoose.connect(`${process.env.DATABASE_CONNECTION}`);
   const conn = mongoose.connection;
-  conn.on("error", console.log.bind(console, 'Houve um erro na conexao.'))
+  conn.on("error", console.log.bind(console, "Houve um erro na conexao."));
   conn.once("open", () => {
-    console.log('Sucesso na conexao.')
-  })
-
+    console.log("Sucesso na conexao.");
+  });
 }
